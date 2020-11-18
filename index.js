f48fe99fcec980c949edcc35029fba85bd6e9469
Jp1200 <jprkr96@gmail.com>
2020-11-18 09:44:16 -0600

    animations and js added

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("javascript loaded...")
+});
+
+const ball1 = document.getElementById("Vector_55");
+
+const btn = document.getElementById('loadtest');
+btn.addEventListener("click", ()=>{
+    console.log("clicked")
+})
