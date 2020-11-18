204735fcc067c524bf556506903ee8e3d6073881
Jp1200 <jprkr96@gmail.com>
2020-11-18 10:13:09 -0600

    animation 2 start

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(){
     console.log("javascript loaded...")
+    const ball1 = document.getElementById("Vector_55");
+    const icon = document.getElementById("load_icon");
+    const btn = document.getElementById('loadtest');
+    const btn2 = document.getElementById('loadtest2');
+    btn.addEventListener("click", ()=>{
+        icon.id = "load_icon_progress";
+        console.log("clicked")
+    })
+    btn2.addEventListener("click", ()=>{
+        icon.id = "load_icon_progress2";
+        
+    })
 });
 
-const ball1 = document.getElementById("Vector_55");
-
-const btn = document.getElementById('loadtest');
-btn.addEventListener("click", ()=>{
-    console.log("clicked")
-})
